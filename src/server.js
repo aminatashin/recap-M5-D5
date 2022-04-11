53924fbd1946ad759d84589806e8084ec2249a1f
Amin Atashkhayer <amin.atashin@gmail.com>
2022-04-11 18:03:36 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,10 +13,10 @@ import {
 import createError from "http-errors";
 // -----------------------------------------
 const server = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 const publicPath = join(process.cwd(), "./public");
 // -------------------------------------------
-const whiteList = [process.env.FE_DEV_URL];
+const whiteList = [process.env.FE_DEV_URL, process.env.FE_REM_URL];
 server.use(express.static(publicPath));
 server.use(
   cors({
