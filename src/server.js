097cb1dbb7df932ac778c28a1a08c1a029c33c95
Amin Atashkhayer <amin.atashin@gmail.com>
2022-04-11 16:56:20 +0200

    add recap

--- src/server.js
+++ src/server.js
@@ -3,24 +3,38 @@ import cors from "cors";
 import myRouter from "./authors/authors.js";
 import blogsRouter from "./blogs/blogs.js";
 import fileRouter from "./file/file.js";
+import { join } from "path";
 import {
   badRequestError,
   unauthurizedError,
   notFoundError,
   genericError,
 } from "./errorHandler/errorHandler.js";
+import createError from "http-errors";
 // -----------------------------------------
 const server = express();
-const port = 3001;
-// -------------------------------------------
-server.use(cors());
-server.use(express.json());
+const port = process.env.port || 3001;
 const publicPath = join(process.cwd(), "./public");
+// -------------------------------------------
+const whiteList = [process.env.FE_DEV_URL];
+server.use(express.static(publicPath));
+server.use(
+  cors({
+    origin: function (origin, next) {
+      if (!origin || whiteList.indexof(origin !== -1)) {
+        next(null, true);
+      } else {
+        next(createError(400, "CORS Error"));
+      }
+    },
+  })
+);
+server.use(express.json());
+
 // ------------------END POINT-----------------
 server.use("/authors", myRouter);
 server.use("/blogs", blogsRouter);
 server.use("/file", fileRouter);
-server.use(express.static(publicPath));
 
 // ---------------------------------------------
 server.use(badRequestError);
