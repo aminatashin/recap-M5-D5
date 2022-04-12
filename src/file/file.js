404b0d4827b3367f56a42b6b2d9ca82248ab03c9
Amin Atashkhayer <amin.atashin@gmail.com>
2022-04-12 06:00:35 +0200

    cloudinary

--- src/file/file.js
+++ src/file/file.js
@@ -1,10 +1,14 @@
 import express from "express";
 import multer from "multer";
 import { saveAvatar } from "../tools/fs.js";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 // ------------------------------------
 const fileRouter = express.Router();
 // ------------------------------------
-
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({ cloudinary, params: { folder: "M5-D6" } }),
+}).single("avatar");
 fileRouter.post(
   "/singleUpload",
   multer().single("avatar"),
@@ -18,4 +22,11 @@ fileRouter.post(
     }
   }
 );
+fileRouter.post("/cloudinaryUploader", cloudinaryUploader, (req, res, next) => {
+  try {
+    console.log(req.file);
+    res.send();
+  } catch (error) {}
+});
+
 export default fileRouter;
